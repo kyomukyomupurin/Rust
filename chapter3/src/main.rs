5bbb0fb4e9b768383f4cebff4208032fc79a2bb5
kyomukyomupurin <murakami1728@gmail.com>
2020-09-03 15:36:46 +0900

    :tada: Initial commit

---
+++ chapter3/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = 5 + 10;
+
+    let diff = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let rem = 43 % 5;
+
+    let t = true;
+
+    dbg!(sum, diff, product, rem, t);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    dbg!(tup);
+}
