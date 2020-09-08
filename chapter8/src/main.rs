35e0fd520f65c02795c9c940add2f173870ed749
kyomukyomupurin <murakami1728@gmail.com>
2020-09-08 20:36:35 +0900

    :sparkles: Update

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -45,9 +45,22 @@ fn main() {
 
     let s = format!("{}-{}-{}", s1, s2, s3);
     println!("{}", s);
-
+/*
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+*/
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
 }
