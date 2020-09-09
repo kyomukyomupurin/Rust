d78fc1600e154523e0f27e9a2b46215fd4947005
kyomukyomupurin <murakami1728@gmail.com>
2020-09-09 21:39:19 +0900

    :sparkles: Update

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -60,7 +60,22 @@ fn main() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
-
+/*
     let team_name = String::from("Blue");
     let score = scores.get(&team_name);
+*/
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
